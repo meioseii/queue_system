c58ddcea24dcacff40543ac0d3e594bacb092463
austinmoreno <moreno.austin12@gmail.com>
2025-04-11 20:38:59 +0800

    added regex for input boxes

--- app/(auth)/Register.tsx
+++ app/(auth)/Register.tsx
@@ -8,7 +8,17 @@ import {
 } from "@expo-google-fonts/poppins";
 import { TextInput, Button, Text } from "react-native-paper";
 import { Icon, MD3Colors } from "react-native-paper";
-import { View, StyleSheet, ScrollView, TouchableOpacity } from "react-native";
+import {
+  View,
+  StyleSheet,
+  ScrollView,
+  TouchableOpacity,
+  KeyboardAvoidingView,
+  Platform,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from "react-native";
+
 import { useEffect, useState } from "react";
 import { useNavigation } from "@react-navigation/native";
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
@@ -41,6 +51,7 @@ export default function Register() {
   const {
     control,
     handleSubmit,
+    watch,
     formState: { errors },
   } = useForm<FormData>();
 
@@ -65,241 +76,267 @@ export default function Register() {
   }
 
   return (
-    <View style={styles.container}>
-      <ScrollView contentContainerStyle={{ flexGrow: 1, paddingBottom: 40 }}>
-        <StatusBar hidden={true}></StatusBar>
-        <TouchableOpacity
-          style={styles.backContainer}
-          onPress={() => navigation.pop()}
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <KeyboardAvoidingView
+        style={styles.container}
+        behavior={Platform.OS === "ios" ? "padding" : "height"} // adjust based on header height, etc.
+      >
+        <ScrollView
+          contentContainerStyle={{ flexGrow: 1, paddingBottom: 40 }}
+          keyboardShouldPersistTaps="handled"
         >
-          <Icon source="arrow-left" color="#FF9500" size={16} />
-          <Text style={styles.backText}>Back</Text>
-        </TouchableOpacity>
-        <View style={styles.iconContainer}>
-          <Image
-            style={styles.icon}
-            source={require("../../assets/images/iQueueImage.svg")}
-            placeholder={{ blurhash }}
-            contentFit="cover"
-          />
-          <Text style={styles.iconText}>iQUEUE</Text>
-        </View>
-        <View style={styles.formContainer}>
-          <Controller
-            control={control}
-            name="firstName"
-            rules={{ required: "First name is required" }}
-            render={({ field: { onChange, value } }) => (
-              <TextInput
-                label="First Name"
-                mode="outlined"
-                outlineColor="#000"
-                activeOutlineColor="#FF9500"
-                textColor="#000"
-                style={[styles.input, { backgroundColor: "transparent" }]}
-                contentStyle={{ fontFamily: "Poppins_400Regular" }}
-                value={value}
-                onChangeText={onChange}
-                error={!!errors.firstName}
-              />
-            )}
-          />
-          {errors.firstName && (
-            <Text
-              style={{
-                color: "red",
-                marginLeft: 40,
-                fontFamily: "Poppins_400Regular",
-              }}
-            >
-              {errors.firstName.message}
-            </Text>
-          )}
-
-          <Controller
-            control={control}
-            name="lastName"
-            rules={{ required: "Last name is required" }}
-            render={({ field: { onChange, value } }) => (
-              <TextInput
-                label="Last Name"
-                mode="outlined"
-                outlineColor="#000"
-                activeOutlineColor="#FF9500"
-                textColor="#000"
-                style={[styles.input, { backgroundColor: "transparent" }]}
-                contentStyle={{ fontFamily: "Poppins_400Regular" }}
-                value={value}
-                onChangeText={onChange}
-                error={!!errors.lastName}
-              />
-            )}
-          />
-          {errors.lastName && (
-            <Text
-              style={{
-                color: "red",
-                marginLeft: 40,
-                fontFamily: "Poppins_400Regular",
-              }}
-            >
-              {errors.lastName.message}
-            </Text>
-          )}
-
-          <Controller
-            control={control}
-            name="username"
-            rules={{ required: "Username is required" }}
-            render={({ field: { onChange, value } }) => (
-              <TextInput
-                label="Username"
-                mode="outlined"
-                outlineColor="#000"
-                activeOutlineColor="#FF9500"
-                textColor="#000"
-                style={[styles.input, { backgroundColor: "transparent" }]}
-                contentStyle={{ fontFamily: "Poppins_400Regular" }}
-                value={value}
-                onChangeText={onChange}
-                error={!!errors.username}
-              />
-            )}
-          />
-          {errors.username && (
-            <Text
-              style={{
-                color: "red",
-                marginLeft: 40,
-                fontFamily: "Poppins_400Regular",
-              }}
-            >
-              {errors.username.message}
-            </Text>
-          )}
-
-          <Controller
-            control={control}
-            name="email"
-            rules={{ required: "Email is required" }}
-            render={({ field: { onChange, value } }) => (
-              <TextInput
-                label="Email"
-                mode="outlined"
-                outlineColor="#000"
-                activeOutlineColor="#FF9500"
-                textColor="#000"
-                style={[styles.input, { backgroundColor: "transparent" }]}
-                contentStyle={{ fontFamily: "Poppins_400Regular" }}
-                value={value}
-                onChangeText={onChange}
-                error={!!errors.email}
-              />
-            )}
-          />
-          {errors.email && (
-            <Text
-              style={{
-                color: "red",
-                marginLeft: 40,
-                fontFamily: "Poppins_400Regular",
-              }}
-            >
-              {errors.email.message}
-            </Text>
-          )}
-
-          <Controller
-            control={control}
-            name="password"
-            rules={{ required: "Password is required" }}
-            render={({ field: { onChange, value } }) => (
-              <TextInput
-                label="Password"
-                secureTextEntry={!seePass}
-                mode="outlined"
-                right={
-                  <TextInput.Icon
-                    icon={seePass ? "eye" : "eye-off"}
-                    onPress={() => setSeePass(!seePass)}
-                  />
-                }
-                outlineColor="#000"
-                activeOutlineColor="#FF9500"
-                textColor="#000"
-                style={[styles.input, { backgroundColor: "transparent" }]}
-                value={value}
-                onChangeText={onChange}
-                error={!!errors.password}
-              />
-            )}
-          />
-          {errors.password && (
-            <Text
-              style={{
-                color: "red",
-                marginLeft: 40,
-                fontFamily: "Poppins_400Regular",
-              }}
-            >
-              {errors.password.message}
-            </Text>
-          )}
-
-          <Controller
-            control={control}
-            name="confirmPassword"
-            rules={{ required: "Password must be matched" }}
-            render={({ field: { onChange, value } }) => (
-              <TextInput
-                label="Confirm Password"
-                secureTextEntry={!seeConfirmPass}
-                mode="outlined"
-                right={
-                  <TextInput.Icon
-                    icon={seeConfirmPass ? "eye" : "eye-off"}
-                    onPress={() => setSeeConfirmPass(!seeConfirmPass)}
-                  />
-                }
-                outlineColor="#000"
-                activeOutlineColor="#FF9500"
-                textColor="#000"
-                style={[styles.input, { backgroundColor: "transparent" }]}
-                value={value}
-                onChangeText={onChange}
-                error={!!errors.confirmPassword}
-              />
-            )}
-          />
-          {errors.confirmPassword && (
-            <Text
-              style={{
-                color: "red",
-                marginLeft: 40,
-                fontFamily: "Poppins_400Regular",
-              }}
-            >
-              {errors.confirmPassword.message}
-            </Text>
-          )}
-        </View>
-        <View>
-          <Button
-            mode="contained"
-            onPress={handleSubmit(onSubmit)}
-            style={[styles.input, { bottom: 20 }]}
-            buttonColor="#FF9500"
-            loading={loading}
-            disabled={loading}
+          <StatusBar hidden={true}></StatusBar>
+          <TouchableOpacity
+            style={styles.backContainer}
+            onPress={() => navigation.pop()}
           >
-            <Text style={{ fontFamily: "Poppins_700Bold", color: "white" }}>
-              REGISTER
-            </Text>
-          </Button>
-          <View style={{ height: 100 }}></View>
-        </View>
-      </ScrollView>
-    </View>
+            <Icon source="arrow-left" color="#FF9500" size={16} />
+            <Text style={styles.backText}>Back</Text>
+          </TouchableOpacity>
+          <View style={styles.iconContainer}>
+            <Image
+              style={styles.icon}
+              source={require("../../assets/images/iQueueImage.svg")}
+              placeholder={{ blurhash }}
+              contentFit="cover"
+            />
+            <Text style={styles.iconText}>iQUEUE</Text>
+          </View>
+          <View style={styles.formContainer}>
+            <Controller
+              control={control}
+              name="firstName"
+              rules={{ required: "First name is required" }}
+              render={({ field: { onChange, value } }) => (
+                <TextInput
+                  label="First Name"
+                  mode="outlined"
+                  outlineColor="#000"
+                  activeOutlineColor="#FF9500"
+                  textColor="#000"
+                  style={[styles.input, { backgroundColor: "transparent" }]}
+                  contentStyle={{ fontFamily: "Poppins_400Regular" }}
+                  value={value}
+                  onChangeText={onChange}
+                  error={!!errors.firstName}
+                />
+              )}
+            />
+            {errors.firstName && (
+              <Text
+                style={{
+                  color: "red",
+                  marginLeft: 40,
+                  fontFamily: "Poppins_400Regular",
+                }}
+              >
+                {errors.firstName.message}
+              </Text>
+            )}
+
+            <Controller
+              control={control}
+              name="lastName"
+              rules={{ required: "Last name is required" }}
+              render={({ field: { onChange, value } }) => (
+                <TextInput
+                  label="Last Name"
+                  mode="outlined"
+                  outlineColor="#000"
+                  activeOutlineColor="#FF9500"
+                  textColor="#000"
+                  style={[styles.input, { backgroundColor: "transparent" }]}
+                  contentStyle={{ fontFamily: "Poppins_400Regular" }}
+                  value={value}
+                  onChangeText={onChange}
+                  error={!!errors.lastName}
+                />
+              )}
+            />
+            {errors.lastName && (
+              <Text
+                style={{
+                  color: "red",
+                  marginLeft: 40,
+                  fontFamily: "Poppins_400Regular",
+                }}
+              >
+                {errors.lastName.message}
+              </Text>
+            )}
+
+            <Controller
+              control={control}
+              name="username"
+              rules={{ required: "Username is required" }}
+              render={({ field: { onChange, value } }) => (
+                <TextInput
+                  label="Username"
+                  mode="outlined"
+                  outlineColor="#000"
+                  activeOutlineColor="#FF9500"
+                  textColor="#000"
+                  style={[styles.input, { backgroundColor: "transparent" }]}
+                  contentStyle={{ fontFamily: "Poppins_400Regular" }}
+                  value={value}
+                  onChangeText={onChange}
+                  error={!!errors.username}
+                />
+              )}
+            />
+            {errors.username && (
+              <Text
+                style={{
+                  color: "red",
+                  marginLeft: 40,
+                  fontFamily: "Poppins_400Regular",
+                }}
+              >
+                {errors.username.message}
+              </Text>
+            )}
+
+            <Controller
+              control={control}
+              name="email"
+              rules={{
+                required: "Email is required",
+                pattern: {
+                  value: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/,
+                  message: "Please enter a valid email address",
+                },
+              }}
+              render={({ field: { onChange, value } }) => (
+                <TextInput
+                  label="Email"
+                  mode="outlined"
+                  outlineColor="#000"
+                  activeOutlineColor="#FF9500"
+                  textColor="#000"
+                  style={[styles.input, { backgroundColor: "transparent" }]}
+                  contentStyle={{ fontFamily: "Poppins_400Regular" }}
+                  value={value}
+                  onChangeText={onChange}
+                  error={!!errors.email}
+                />
+              )}
+            />
+            {errors.email && (
+              <Text
+                style={{
+                  color: "red",
+                  marginLeft: 40,
+                  fontFamily: "Poppins_400Regular",
+                }}
+              >
+                {errors.email.message}
+              </Text>
+            )}
+
+            <Controller
+              control={control}
+              name="password"
+              rules={{
+                required: "Password is required",
+                pattern: {
+                  value:
+                    /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*\W)(?!.* ).{8,16}$/,
+                  message:
+                    "Password must be 8-16 characters, include uppercase, lowercase, number, and symbol",
+                },
+              }}
+              render={({ field: { onChange, value } }) => (
+                <TextInput
+                  label="Password"
+                  secureTextEntry={!seePass}
+                  mode="outlined"
+                  right={
+                    <TextInput.Icon
+                      icon={seePass ? "eye" : "eye-off"}
+                      onPress={() => setSeePass(!seePass)}
+                    />
+                  }
+                  outlineColor="#000"
+                  activeOutlineColor="#FF9500"
+                  textColor="#000"
+                  style={[styles.input, { backgroundColor: "transparent" }]}
+                  value={value}
+                  onChangeText={onChange}
+                  error={!!errors.password}
+                />
+              )}
+            />
+            {errors.password && (
+              <Text
+                style={{
+                  color: "red",
+                  marginLeft: 40,
+                  fontFamily: "Poppins_400Regular",
+                }}
+              >
+                {errors.password.message}
+              </Text>
+            )}
+
+            <Controller
+              control={control}
+              name="confirmPassword"
+              rules={{
+                required: "Please confirm your password",
+                validate: (value) =>
+                  value === watch("password") || "Passwords do not match",
+              }}
+              render={({ field: { onChange, value } }) => (
+                <TextInput
+                  label="Confirm Password"
+                  secureTextEntry={!seeConfirmPass}
+                  mode="outlined"
+                  right={
+                    <TextInput.Icon
+                      icon={seeConfirmPass ? "eye" : "eye-off"}
+                      onPress={() => setSeeConfirmPass(!seeConfirmPass)}
+                    />
+                  }
+                  outlineColor="#000"
+                  activeOutlineColor="#FF9500"
+                  textColor="#000"
+                  style={[styles.input, { backgroundColor: "transparent" }]}
+                  value={value}
+                  onChangeText={onChange}
+                  error={!!errors.confirmPassword}
+                />
+              )}
+            />
+            {errors.confirmPassword && (
+              <Text
+                style={{
+                  color: "red",
+                  marginLeft: 40,
+                  fontFamily: "Poppins_400Regular",
+                }}
+              >
+                {errors.confirmPassword.message}
+              </Text>
+            )}
+          </View>
+          <View>
+            <Button
+              mode="contained"
+              onPress={handleSubmit(onSubmit)}
+              style={[styles.input, { bottom: 20 }]}
+              buttonColor="#FF9500"
+              loading={loading}
+              disabled={loading}
+            >
+              <Text style={{ fontFamily: "Poppins_700Bold", color: "white" }}>
+                REGISTER
+              </Text>
+            </Button>
+            <View style={{ height: 100 }}></View>
+          </View>
+        </ScrollView>
+      </KeyboardAvoidingView>
+    </TouchableWithoutFeedback>
   );
 }
 
