c57283bfbd35a64e0a3d23a343610196904f87f4
austinmoreno <moreno.austin12@gmail.com>
2025-04-23 03:20:44 +0800

    added flash list to render menu categories

--- app/(app)/Menu.tsx
+++ app/(app)/Menu.tsx
@@ -6,19 +6,20 @@ import {
   Poppins_400Regular,
   Poppins_700Bold,
 } from "@expo-google-fonts/poppins";
-import { TextInput, Button, Text } from "react-native-paper";
+import { Text } from "react-native-paper";
 import { View, StyleSheet, TouchableOpacity } from "react-native";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useNavigation } from "@react-navigation/native";
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 import { AppStackParamList } from "../app-types";
 import { useAuthStore } from "../store/auth-store";
+import { FlashList } from "@shopify/flash-list";
 
 const blurhash =
   "|rF?hV%2WCj[ayj[a|j[az_NaeWBj@ayfRayfQfQM{M|azj[azf6fQfQfQIpWXofj[ayj[j[fQayWCoeoeaya}j[ayfQa{oLj?j[WVj[ayayj[fQoff7azayj[ayj[j[ayofayayayj[fQj[ayayj[ayfjj[j[ayjuayj[";
 
 export default function Menu() {
-  type Navigation = NativeStackNavigationProp<AppStackParamList, "Login">;
+  type Navigation = NativeStackNavigationProp<AppStackParamList, "Menu">;
   const navigation = useNavigation<Navigation>();
   const { logout } = useAuthStore();
 
@@ -41,12 +42,68 @@ export default function Menu() {
     logout();
   };
 
+  const categories = [
+    {
+      name: "Main Dish",
+      image: "https://iqueue-bucket.s3.amazonaws.com/uploads/Beef Rendang.jpg",
+    },
+    {
+      name: "Side Dish and Appetizer",
+      image: "https://iqueue-bucket.s3.amazonaws.com/uploads/Nem Ran.png",
+    },
+    {
+      name: "Refresher",
+      image: "https://iqueue-bucket.s3.amazonaws.com/uploads/Tanglad Twist.png",
+    },
+    {
+      name: "Refresher",
+      image: "https://iqueue-bucket.s3.amazonaws.com/uploads/Tanglad Twist.png",
+    },
+    {
+      name: "Refresher",
+      image: "https://iqueue-bucket.s3.amazonaws.com/uploads/Tanglad Twist.png",
+    },
+    {
+      name: "Refresher",
+      image: "https://iqueue-bucket.s3.amazonaws.com/uploads/Tanglad Twist.png",
+    },
+    {
+      name: "Refresher",
+      image: "https://iqueue-bucket.s3.amazonaws.com/uploads/Tanglad Twist.png",
+    },
+    {
+      name: "Refresher",
+      image: "https://iqueue-bucket.s3.amazonaws.com/uploads/Tanglad Twist.png",
+    },
+    {
+      name: "Refresher",
+      image: "https://iqueue-bucket.s3.amazonaws.com/uploads/Tanglad Twist.png",
+    },
+  ];
+
   return (
-    <View>
+    <View style={styles.container}>
+      <View style={styles.menuHeader}>
+        <Text style={styles.menuHeaderText}>Menu</Text>
+      </View>
       <StatusBar hidden={false} backgroundColor="#FF9500"></StatusBar>
-      <TouchableOpacity onPress={() => onLogout()}>
-        <Text style={{ marginTop: 40 }}>Menu</Text>
-      </TouchableOpacity>
+      <FlashList
+        data={categories}
+        numColumns={2}
+        renderItem={({ item }) => (
+          <View style={styles.card}>
+            <Image source={{ uri: item.image }} style={styles.cardImage} />
+            <View>
+              <Text style={styles.cardTitle}>{item.name}</Text>
+            </View>
+            <TouchableOpacity style={styles.cardButton}>
+              <Text style={styles.cardButtonText}>View More</Text>
+            </TouchableOpacity>
+          </View>
+        )}
+        estimatedItemSize={200}
+        contentContainerStyle={styles.flashListContent}
+      />
     </View>
   );
 }
@@ -55,34 +112,57 @@ const styles = StyleSheet.create({
   container: {
     height: "100%",
     backgroundColor: "#FAF9F6",
-  },
-  iconContainer: {
-    backgroundColor: "#FAF9F6",
-    height: "40%",
-    padding: "10%",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  icon: {
-    width: 120,
-    height: 120,
-  },
-  iconText: {
-    color: "#FF9500",
-    fontSize: 30,
-    fontFamily: "Poppins_700Bold",
-  },
-  checkMessage: {
+    marginTop: 36,
     display: "flex",
-    flexDirection: "column",
-    justifyContent: "center",
-    alignItems: "center",
-    gap: 20,
-    bottom: 50,
   },
-  successMessage: {
+  card: {
+    flex: 1,
+    margin: 10,
+    backgroundColor: "#FFF",
+    borderRadius: 10,
+    overflow: "hidden",
+    elevation: 3,
+  },
+  cardImage: {
+    width: "100%",
+    height: 150,
+  },
+  cardTitle: {
+    position: "absolute",
+    bottom: 0,
+    width: "100%",
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+    color: "#FFF",
     fontFamily: "Poppins_400Regular",
-    letterSpacing: 0,
-    bottom: 10,
+    fontSize: 12,
+    textAlign: "center",
+    paddingVertical: 5,
+  },
+  cardButton: {
+    backgroundColor: "#FF9500",
+    paddingVertical: 10,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  cardButtonText: {
+    fontFamily: "Poppins_700Bold",
+    fontSize: 14,
+    color: "#FFF",
+  },
+  menuHeader: {
+    backgroundColor: "#FF9500",
+    elevation: 5,
+    paddingLeft: 15,
+    paddingVertical: 10,
+    borderRadius: 0,
+  },
+  menuHeaderText: {
+    fontFamily: "Poppins_400Regular",
+    fontSize: 16,
+    color: "#fff",
+  },
+  flashListContent: {
+    paddingTop: 10,
+    paddingBottom: 120,
   },
 });
